53637aaaf031ae8143e6f6301aef1f362ae0e6ba
Mariana Capuñay <mariana.capunay@utec.edu.pe>
2023-05-28 12:16:17 -0500

    avl

--- main.cpp
+++ main.cpp
@@ -1,25 +1,58 @@
-
-//realizando una hash table
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
-#include "SHA256.h"
-#include "lectura.h"
+#include <vector>
+#include "structs/avl/avl.h"
+#include "structs/blockchain/blockchain.h"
+//#include "structs/tabla_hash/chainhash.h"
+
+//#include "SHA256.h"
+//#include "lectura.h"
 
 using namespace std;
 
 int main(){
-        /*
-                string s = "hello world";
-        SHA256 sha;
-        sha.update(s);
-        uint8_t * digest = sha.digest();
+    std::ifstream file("test1.csv");
+    if (!file.is_open()) {
+        std::cout << "Failed to open the file." << std::endl;
+        return 1;
+    }
+    //añandiendo arbol y isertando ----------------------------------
+    AVLTree2<int,int> avl;
+    int count =0;
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string item;
+        std::getline(ss, item, ',');
+        //std::cout<< item<<" ,";
+        std::getline(ss, item, ',');
+        //std::cout << item << " ,";
+        //monto
+        std::getline(ss, item, ',');
+        int number=stoi(item);
+        avl.insert(count,number); // key: nro bloque - value: monto
+        //--fecha
+        std::getline(ss, item, ',');
+        count ++;
+        //std::cout << item<<std::endl;
 
-        std::cout << SHA256::toString(digest) << std::endl;
+    }
 
-        delete[] digest; 
-        return 0;
-        */
+    file.close();
+    avl.displayPretty();
+    cout<<endl;
+    vector <int> result = avl.search_by_range(0,5);
+    for (auto x:result) cout<<x<<" ";
 
-       readCSV("test1.csv");
-        return 0;
+
+
+
+
+    //----------------------------------------------------------------
+        
+
+    return 0;
 }
